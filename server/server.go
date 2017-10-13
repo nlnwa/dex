24ad387700354bb0e9de82dce56f652eb74df92a
John Erik Halse <johnerikhalse@gmail.com>
2017-10-13 14:34:36 +0200

    pilt connector

--- server/server.go
+++ server/server.go
@@ -35,6 +35,7 @@ import (
 	"github.com/dexidp/dex/connector/oidc"
 	"github.com/dexidp/dex/connector/saml"
 	"github.com/dexidp/dex/storage"
+	"github.com/coreos/dex/connector/pilt"
 )
 
 // LocalConnector is the local passwordDB connector which is an internal
@@ -446,6 +447,7 @@ var ConnectorsConfig = map[string]func() ConnectorConfig{
 	"linkedin":        func() ConnectorConfig { return new(linkedin.Config) },
 	"microsoft":       func() ConnectorConfig { return new(microsoft.Config) },
 	"bitbucket-cloud": func() ConnectorConfig { return new(bitbucketcloud.Config) },
+	"pilt":         func() ConnectorConfig { return new(pilt.Config) },
 	// Keep around for backwards compatibility.
 	"samlExperimental": func() ConnectorConfig { return new(saml.Config) },
 }
